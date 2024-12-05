ac163b9b017b0cdf0ac788dfc4e53d29389015dc
Brian Taylor <brian.taylor@redargyle.com>
2024-12-05 09:30:26 -0500

    Remove unnecessary

--- commitlint.config.cjs
+++
@@ -1,8 +0,0 @@
-const { RuleConfigSeverity } = require('@commitlint/types');
-
-module.exports = {
-  parserPreset: 'conventional-changelog-conventionalcommits',
-  rules: {
-    'body-empty': [RuleConfigSeverity.Warning, 'always'],
-  },
-};
